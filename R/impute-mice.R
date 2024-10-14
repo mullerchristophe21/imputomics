ad51b83638e0afc22c8075e977c25cb82d9aa0eb
Christophe Muller <mullec@ethz.ch>
2024-10-14 09:25:08 +0200

    namespace - mice.impute.drf

--- R/impute-mice.R
+++ R/impute-mice.R
@@ -11,10 +11,17 @@ eval_mice_calls <- function(missdf, method, ...) {
   mice::complete(imputed)
 }
 
+#' \strong{mice-drf}
+#'
+#' Multiple Imputation by Chained Equations.
+#'
+#' A function to replace \code{NA} in the data frame by random forest
+#' imputations as provided by [mice::mice()].
+#'
+#'
+#' @export
+mice.impute.DRF <- function(y, ry, x, wy = NULL,min.node.size=1, num.features=10,  num.trees=10 , ...) {
 
-mice.impute.DRF<- function (y, ry, x, wy = NULL,min.node.size=1, num.features=10,  num.trees=10 , ...)
-{
-  #install.on.demand("drf", ...)
   require(drf)
   if (is.null(wy)) {
     wy <- !ry
@@ -26,21 +33,13 @@ mice.impute.DRF<- function (y, ry, x, wy = NULL,min.node.size=1, num.features=10
   xobs <- x[ry, , drop = FALSE]
   xmis <- x[wy, , drop = FALSE]
   yobs <- as.matrix(y[ry])
-  
-  # args<-list(...)
-  # if ("m" %in% names(args)){
-  #   m<-args$m
-  # }else{
-  #   m=1
-  # }
+
   m<-1
   
   
   fit <- drf(Y=yobs, X=xobs,num.trees=num.trees, num.features=num.features, compute.oob.predictions = F, min.node.size=min.node.size)
   DRFw <- predict(fit, newdata=xmis)$weights # These are the nodes now
   impute <- vapply(1:nrow(xmis), function(s) yobs[sample(1:nrow(yobs), size=1, replace=T, prob=DRFw[s,]), ], numeric(m))  # sample one observation per xmis
-  
-  
   
   
   impute
